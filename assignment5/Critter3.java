b2c6ba80c8d3865716d716fb75022a143cfe7896
RonZhang724 <ronzhang@utexas.edu>
2018-04-03 22:44:47 -0500

    added header for all the java files

--- assignment5/Critter3.java
+++ assignment5/Critter3.java
@@ -1,5 +1,16 @@
 package assignment5;
-
+/* CRITTERS Critter.java
+ * EE422C Project 5 submission by
+ * Replace <...> with your actual data.
+ * Ronghao Zhang
+ * rz4453
+ * 15505
+ * Wenxuan Wu
+ * ww6726
+ * 15505
+ * Slip days used: <0>
+ * Spring 2018
+ */
 import assignment5.Critter.CritterShape;
 
 /*
